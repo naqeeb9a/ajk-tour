83d703e82c742513e3f8543fd12e375a68bf2dd7
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-11-15 17:08:25 +0500

    Hassan ~ Select detail page modification

--- lib/app_screens/selected_detail_page.dart
+++ lib/app_screens/selected_detail_page.dart
@@ -1,5 +1,4 @@
-import 'dart:ui';
-
+import 'package:ajk_tour/utils/app_routes.dart';
 import 'package:ajk_tour/utils/config.dart';
 import 'package:ajk_tour/widgets/dynamic_sizes.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -8,6 +7,7 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class SelectedDetailPage extends StatelessWidget {
   final _pageController = PageController();
+
   SelectedDetailPage({
     Key? key,
   }) : super(key: key);
@@ -33,28 +33,31 @@ class SelectedDetailPage extends StatelessWidget {
             ),
           ),
           SafeArea(
-            child: Container(
-              height: 57.6,
-              margin: const EdgeInsets.only(top: 28.8, left: 28.8, right: 28.8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Container(
-                      height: 57.6,
-                      width: 57.6,
-                      padding: const EdgeInsets.all(18),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(9.6),
-                        color: const Color(0x10000000),
-                      ),
-                      child: const Icon(Icons.arrow_back),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: dynamicHeight(context, .01),
+                horizontal: dynamicWidth(context, .04),
+              ),
+              child: InkWell(
+                onTap: () {
+                  pop(context);
+                },
+                child: Container(
+                  width: dynamicWidth(context, .14),
+                  height: dynamicHeight(context, .06),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      dynamicWidth(context, .02),
+                    ),
+                    color: myBlack,
+                  ),
+                  child: const Center(
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: myWhite,
                     ),
                   ),
-                ],
+                ),
               ),
             ),
           ),
@@ -62,43 +65,52 @@ class SelectedDetailPage extends StatelessWidget {
             bottom: 0,
             left: 0,
             child: Container(
-              decoration: BoxDecoration(color: Colors.white.withOpacity(0)),
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(.6),
+              ),
               width: dynamicWidth(context, 1),
-              padding: const EdgeInsets.only(
-                  left: 28.8, bottom: 48, right: 28.8, top: 20),
+              padding: EdgeInsets.symmetric(
+                vertical: dynamicHeight(context, .04),
+                horizontal: dynamicWidth(context, .06),
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SmoothPageIndicator(
                     controller: _pageController,
                     count: tabImages1.length,
-                    effect: const ExpandingDotsEffect(
-                        activeDotColor: Color(0xFFFFFFFF),
-                        dotColor: Color(0xFFababab),
-                        dotHeight: 4.8,
-                        dotWidth: 6,
-                        spacing: 4.8),
+                    effect: ExpandingDotsEffect(
+                      activeDotColor: myWhite,
+                      dotColor: myWhite.withOpacity(.6),
+                      dotHeight: dynamicHeight(context, .01),
+                      dotWidth: dynamicWidth(context, .02),
+                      spacing: dynamicWidth(context, .014),
+                    ),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 19.2),
-                    child: Text(
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: dynamicHeight(context, .02),
+                    ),
+                    child: const Text(
                       "recommendedModel tagLine",
                       maxLines: 2,
                       overflow: TextOverflow.fade,
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 19.2),
-                    child: Text(
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: dynamicHeight(context, .02),
+                    ),
+                    child: const Text(
                       "recommendedModel description",
                       maxLines: 3,
                       overflow: TextOverflow.fade,
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
-                  const SizedBox(
-                    height: 48,
+                  SizedBox(
+                    height: dynamicHeight(context, .05),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -116,20 +128,17 @@ class SelectedDetailPage extends StatelessWidget {
                         ],
                       ),
                       Container(
-                        height: 62.4,
+                        width: dynamicWidth(context, .34),
+                        height: dynamicHeight(context, .06),
                         decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(9.6),
-                            color: Colors.white),
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Container(
-                            padding:
-                                const EdgeInsets.only(left: 28.8, right: 28.8),
-                            child: const FittedBox(
-                              child: Text(
-                                'Explore Now >>',
-                              ),
-                            ),
+                          borderRadius: BorderRadius.circular(
+                            dynamicWidth(context, .02),
+                          ),
+                          color: Colors.white,
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Explore Now >>',
                           ),
                         ),
                       )
