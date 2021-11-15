7b61f3337f7909556fc41841605ee106313a69ac
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-11-15 12:22:48 +0500

    Hassan ~ Project Setup

---
+++ lib/utils/config.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color myWhite = Colors.white;
+const Color myBlack = Color(0xff0c0801);
+const Color noColor = Colors.transparent;
+const Color myGrey = Color(0xffe9ebee);
+const Color primaryBlue = Color(0xff1982c4);
+const Color primaryLiteBlue = Color(0xff47a8e5);
+const Color primaryGreen = Color(0xff2ca896);
+const Color primaryOrange = Color(0xffffa500);
+const Color primaryLiteOrange = Color(0xffffe4b3);
+const Color primaryPurple = Color(0xff4b0082);
