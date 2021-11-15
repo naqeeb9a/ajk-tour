79f23575c84310153857a928ede9daabeee9970e
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-11-15 12:43:21 +0500

    Hassan ~ Project Setup

---
+++ lib/app_screens/states.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class States extends StatefulWidget {
+  const States({Key? key}) : super(key: key);
+
+  @override
+  _StatesState createState() => _StatesState();
+}
+
+class _StatesState extends State<States> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/widgets/boxes.dart
@@ -0,0 +1,8 @@
+import 'package:ajk_tour/widgets/dynamic_sizes.dart';
+import 'package:flutter/material.dart';
+
+Widget stateCard(context, image, text) {
+  return Container(
+    width: dynamicWidth(context, .9),
+  );
+}
