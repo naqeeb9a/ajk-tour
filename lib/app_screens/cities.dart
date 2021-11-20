d96e762b4e8677cc3c277802edc0e5499009fe86
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-11-20 13:12:27 +0500

    Hassan ~ design change

--- lib/app_screens/cities.dart
+++ lib/app_screens/cities.dart
@@ -1,7 +1,6 @@
 import 'package:ajk_tour/api/apis.dart';
 import 'package:ajk_tour/utils/config.dart';
 import 'package:ajk_tour/widgets/boxes.dart';
-import 'package:ajk_tour/widgets/dynamic_sizes.dart';
 import 'package:ajk_tour/widgets/essential_widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -31,42 +30,37 @@ class _CitiesState extends State<Cities> {
       backgroundColor: myGrey,
       floatingActionButton: sosFloatingButton(),
       appBar: customBar(widget.cityName, bgColor: myGrey),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          vertical: dynamicHeight(context, .02),
-        ),
-        child: FutureBuilder(
-          future: ApiData().getInfo("cities/" + widget.index.toString()),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, i) {
-                  return Hero(
-                    tag: i,
-                    child: Material(
-                      color: noColor,
-                      child: cityCard(
-                        context,
+      body: FutureBuilder(
+        future: ApiData().getInfo("cities/" + widget.index.toString()),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, i) {
+                return Hero(
+                  tag: i,
+                  child: Material(
+                    color: noColor,
+                    child: cityCard(
+                      context,
+                      image: snapshot.data[i]["image"].toString(),
+                      text: snapshot.data[i]["name"].toString(),
+                      page: CityDetail(
+                        stateName: snapshot.data[i]["name"].toString(),
                         image: snapshot.data[i]["image"].toString(),
-                        text: snapshot.data[i]["name"].toString(),
-                        page: CityDetail(
-                          stateName: snapshot.data[i]["name"].toString(),
-                          image: snapshot.data[i]["image"].toString(),
-                          index: snapshot.data[i]["id"],
-                          i: i,
-                        ),
+                        index: snapshot.data[i]["id"],
+                        i: i,
                       ),
                     ),
-                  );
-                },
-              );
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
+                  ),
+                );
+              },
             );
-          },
-        ),
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
